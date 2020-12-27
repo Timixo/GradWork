5b9ad065b43986bb4443c488734263506d79d73c
Timixo <51677515+Timixo@users.noreply.github.com>
2020-12-27 15:46:40 +0100

    First push project
    First push of the project + start of implementation hand tracking

---
+++ Physical Interaction/Assets/Scripts/ProxyHand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct ProxyHandBones
+{
+    public Transform _master;
+    public SlaveBone _slave;
+}
+
+public class ProxyHand : MonoBehaviour
+{
+    [SerializeField]
+    private OVRSkeleton _handData = null;
+
+    [SerializeField]
+    private ProxyHandBones[] _proxyHandBones = new ProxyHandBones[24];
+
+    private void Awake()
+    {
+        for (int bone = 0; bone < _proxyHandBones.Length; bone++)
+        {
+            if (_proxyHandBones[bone]._slave && _proxyHandBones[bone]._master)
+                _proxyHandBones[bone]._slave._Master = _proxyHandBones[bone]._master;
+        }
+    }
+
+    void Update()
+    {
+        if (_handData.Bones.Count != _proxyHandBones.Length)
+            return;
+
+        _proxyHandBones[0]._master.position = _handData.Bones[0].Transform.position;
+        _proxyHandBones[0]._master.rotation = _handData.Bones[0].Transform.rotation;
+
+        for (int bone = 1; bone < _handData.Bones.Count; bone++)
+        {
+            _proxyHandBones[bone]._master.rotation = _handData.Bones[bone].Transform.rotation;
+        }
+    }
+}
---
+++ Physical Interaction/Assets/Scripts/SlaveBone.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+public class SlaveBone : MonoBehaviour
+{
+    [SerializeField]
+    private bool _followPosition = false;
+    [SerializeField]
+    private bool _followRotation = true;
+
+    private ConfigurableJoint _joint = null;
+
+    private Transform _master = null;
+
+    private Quaternion _jointRotation;
+    private Quaternion _initLocalRotation;
+    private Quaternion _initGlobalRotation;
+
+    public Transform _Master
+    { 
+        get
+        {
+            return _master;
+        }
+        
+        set
+        {
+            if (!_master)
+                _master = value;
+        }
+    
+    }
+
+    void Start()
+    {
+        if (!_joint)
+            _joint = GetComponent<ConfigurableJoint>();
+
+        Vector3 xAxis = _joint.axis;
+        Vector3 zAxis = Vector3.Cross(_joint.axis, _joint.secondaryAxis).normalized;
+        Vector3 yAxis = Vector3.Cross(zAxis, xAxis).normalized;
+
+        _jointRotation = Quaternion.LookRotation(zAxis, yAxis);
+
+        if (_joint.connectedBody != null && !_joint.configuredInWorldSpace)
+        {
+            _initLocalRotation = _joint.connectedBody.transform.localRotation;
+        }
+        else
+        {
+            _initGlobalRotation = _joint.transform.rotation;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!_master)
+            return;
+
+        if (_followPosition)
+        {
+            if (_joint.connectedBody != null && !_joint.configuredInWorldSpace)
+            {
+                //The joint is not the wrist, may not follow the position, only rotation
+            }
+            else
+            {
+                // these steps are to change the anchor, the pivot point around what the hand will rotate (needs to be your wrist, not the original position)
+                if (_joint.autoConfigureConnectedAnchor)
+                    _joint.autoConfigureConnectedAnchor = false;
+
+                if (_joint.targetPosition != Vector3.zero)
+                    _joint.targetPosition = Vector3.zero;
+
+                _joint.connectedAnchor = _master.position;
+            }
+        }
+
+        if (_followRotation)
+        {
+            Quaternion result;
+            if (_joint.connectedBody != null && !_joint.configuredInWorldSpace)
+            {
+                result = Quaternion.Inverse(_jointRotation);
+                result *= Quaternion.Inverse(_master.localRotation);
+                result *= _initLocalRotation;
+                result *= _jointRotation;
+
+                _joint.targetRotation = result;
+            }
+            else
+            {
+                result = Quaternion.Inverse(_jointRotation);
+                result *= _initGlobalRotation;
+                result *= Quaternion.Inverse(_master.rotation);
+                result *= _jointRotation;
+
+                _joint.targetRotation = result;
+            }
+        }
+    }
+}
