a2d8bea563326290be6b4f366480dbeb017b9be5
Timixo <51677515+Timixo@users.noreply.github.com>
2020-12-30 18:38:47 +0100

    Started implementing physical way
    Started the implementation of the physical way found in papers of doing the physical hand tracking

---
+++ Physical Interaction/Assets/Scripts/RaycastScene/FollowHandData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHandData : MonoBehaviour
+{
+    [SerializeField]
+    private OVRSkeleton _skeletonData = null;
+
+    [SerializeField]
+    private Transform[] _bones = new Transform[24];
+
+    private Rigidbody[] _rigidbodies = new Rigidbody[24];
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int bone = 0; bone < _bones.Length; bone++)
+        {
+            _rigidbodies[bone] = _bones[bone].GetComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((_skeletonData.Bones.Count != _bones.Length) || !_skeletonData.IsDataHighConfidence)
+            return;
+
+        _bones[0].position = _skeletonData.Bones[0].Transform.position;
+        _bones[0].rotation = _skeletonData.Bones[0].Transform.rotation;
+
+        for (int bone = 1; bone < _skeletonData.Bones.Count; bone++)
+        {
+            _bones[bone].rotation = _skeletonData.Bones[bone].Transform.rotation;
+        }
+    }
+}
---
+++ Physical Interaction/Assets/Scripts/RaycastScene/Phalange.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Phalange : MonoBehaviour
+{
+    [Serializable]
+    private struct Contact
+    {
+        public Vector3 _point;
+        public Vector3 _phalanxLocalPoint;
+        public GameObject _object;
+    }
+
+    [Tooltip("Leave empty if the rigidbody is on the same object")]
+    [SerializeField]
+    private Rigidbody _rigidbody = null;
+
+    private Collider _collider = null;
+
+    [SerializeField]
+    private List<Contact> _contacts = new List<Contact>();
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_contacts.Count == 0) _collider.isTrigger = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 contactPoint = new Vector3();
+        foreach (ContactPoint point in collision.contacts)
+            contactPoint += point.point;
+        contactPoint /= collision.contactCount;
+
+        Vector3 direction = collision.collider.transform.position - contactPoint;
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(contactPoint, direction, out hitInfo))
+        {
+            Contact contact = new Contact();
+            contact._point = hitInfo.point;
+            contact._object = hitInfo.collider.gameObject;
+            contact._phalanxLocalPoint = transform.worldToLocalMatrix * hitInfo.point;
+
+            _contacts.Add(contact);
+
+            _collider.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Object"))
+            return;
+
+        GameObject gameObject = other.gameObject;
+        for (int contact = 0; contact < _contacts.Count; contact++)
+        {
+            if (gameObject == _contacts[contact]._object)
+            {
+                _contacts.RemoveAt(contact);
+            }
+        }
+    }
+}
