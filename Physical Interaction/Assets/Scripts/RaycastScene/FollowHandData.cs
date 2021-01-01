07a89a55dd0f08b06d723e38dff4ca89be4c27e9
Timixo <51677515+Timixo@users.noreply.github.com>
2021-01-01 13:39:50 +0100

    Raycastscene work
    Worked further on the raycast way of doing physical hand tracking. Render lines when touching the object from contact point to the center of the object

--- Physical Interaction/Assets/Scripts/RaycastScene/FollowHandData.cs
+++ Physical Interaction/Assets/Scripts/RaycastScene/FollowHandData.cs
@@ -10,14 +10,16 @@ public class FollowHandData : MonoBehaviour
     [SerializeField]
     private Transform[] _bones = new Transform[24];
 
-    private Rigidbody[] _rigidbodies = new Rigidbody[24];
+    [SerializeField]
+    private Phalange _wristPhalange = null;
+    private Phalange[] _phalanges = new Phalange[24];
 
     // Start is called before the first frame update
     void Awake()
     {
         for (int bone = 0; bone < _bones.Length; bone++)
         {
-            _rigidbodies[bone] = _bones[bone].GetComponent<Rigidbody>();
+            _phalanges[bone] = _bones[bone].GetComponent<Phalange>();
         }
     }
 
@@ -27,11 +29,13 @@ public class FollowHandData : MonoBehaviour
         if ((_skeletonData.Bones.Count != _bones.Length) || !_skeletonData.IsDataHighConfidence)
             return;
 
+        _wristPhalange.SetPhalange(_skeletonData.Bones[0].Transform);
         _bones[0].position = _skeletonData.Bones[0].Transform.position;
         _bones[0].rotation = _skeletonData.Bones[0].Transform.rotation;
 
         for (int bone = 1; bone < _skeletonData.Bones.Count; bone++)
         {
+            _phalanges[bone].SetPhalange(_skeletonData.Bones[bone].Transform);
             _bones[bone].rotation = _skeletonData.Bones[bone].Transform.rotation;
         }
     }
--- Physical Interaction/Assets/Scripts/RaycastScene/Phalange.cs
+++ Physical Interaction/Assets/Scripts/RaycastScene/Phalange.cs
@@ -6,12 +6,15 @@ using System.Linq;
 
 public class Phalange : MonoBehaviour
 {
+    private static float _deltaOrigin = 0.0001f;
+
     [Serializable]
     private struct Contact
     {
-        public Vector3 _point;
+        public RaycastHit _hitInfo;
         public Vector3 _phalanxLocalPoint;
         public GameObject _object;
+        public Collider _objectCollider;
     }
 
     [Tooltip("Leave empty if the rigidbody is on the same object")]
@@ -23,14 +26,49 @@ public class Phalange : MonoBehaviour
     [SerializeField]
     private List<Contact> _contacts = new List<Contact>();
 
+    private LineRenderer _lineRenderer = null;
+
+    private Transform _actualPhalangeTransform = null;
+
     private void Awake()
     {
         _collider = GetComponent<Collider>();
+        _lineRenderer = GetComponent<LineRenderer>();
     }
 
     private void FixedUpdate()
     {
-        if (_contacts.Count == 0) _collider.isTrigger = false;
+        for (int i = _contacts.Count - 1; i >= 0; i--)
+        {
+            Contact contact = _contacts[i];
+            Vector3 rayOrigin = contact._hitInfo.point + _deltaOrigin * contact._hitInfo.normal;
+            Vector3 phalanxPos = _actualPhalangeTransform.localToWorldMatrix * contact._phalanxLocalPoint;
+            Vector3 direction = phalanxPos - rayOrigin;
+            float maxDistance = direction.magnitude;
+            direction = direction.normalized;
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, direction, out hitInfo, maxDistance))
+            {
+                if (contact._object == hitInfo.collider.gameObject)
+                {
+
+                }
+                else
+                {
+                    _contacts.Remove(contact);
+                }
+            }
+            else
+            {
+                _contacts.Remove(contact);
+            }
+        }
+        if (_contacts.Count == 0)
+        {
+            _collider.isTrigger = false;
+            _lineRenderer.enabled = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -40,34 +78,50 @@ public class Phalange : MonoBehaviour
             contactPoint += point.point;
         contactPoint /= collision.contactCount;
 
-        Vector3 direction = collision.collider.transform.position - contactPoint;
+        Vector3 objectCenter = collision.collider.bounds.center;
+        Vector3 direction = objectCenter - contactPoint;
+        float maxDistance = direction.magnitude;
+        direction = direction.normalized;
         RaycastHit hitInfo;
 
-        if (Physics.Raycast(contactPoint, direction, out hitInfo))
+        if (Physics.Raycast(contactPoint, direction, out hitInfo, maxDistance))
         {
             Contact contact = new Contact();
-            contact._point = hitInfo.point;
+            contact._hitInfo = hitInfo;
             contact._object = hitInfo.collider.gameObject;
-            contact._phalanxLocalPoint = transform.worldToLocalMatrix * hitInfo.point;
+            contact._phalanxLocalPoint = _actualPhalangeTransform.worldToLocalMatrix * hitInfo.point;
+            contact._objectCollider = hitInfo.collider;
 
             _contacts.Add(contact);
 
             _collider.isTrigger = true;
+
+            _lineRenderer.enabled = true;
+            _lineRenderer.positionCount = 2;
+            _lineRenderer.SetPositions(new Vector3[] { hitInfo.point, hitInfo.point + 10 * direction });
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (!other.CompareTag("Object"))
-            return;
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (!other.CompareTag("Object"))
+    //        return;
 
-        GameObject gameObject = other.gameObject;
-        for (int contact = 0; contact < _contacts.Count; contact++)
-        {
-            if (gameObject == _contacts[contact]._object)
-            {
-                _contacts.RemoveAt(contact);
-            }
-        }
+    //    GameObject gameObject = other.gameObject;
+    //    for (int contact = 0; contact < _contacts.Count; contact++)
+    //    {
+    //        if (gameObject == _contacts[contact]._object)
+    //        {
+    //            Debug.Log("Object exited");
+    //            _lineRenderer.positionCount = 0;
+    //            _lineRenderer.SetPositions(new Vector3[] { });
+    //            _contacts.RemoveAt(contact);
+    //        }
+    //    }
+    //}
+
+    public void SetPhalange(Transform phalange)
+    {
+        _actualPhalangeTransform = phalange;
     }
 }
